8f0c1f74bb7ec38b2b8d9986c591c3faffaf7d5d
Sanjay <imsanju2k@gmail.com>
2022-11-12 15:47:28 +0530

    Assignment-4

---
+++ src/Assignment4/FindMaxOccurringChar.java
@@ -0,0 +1,27 @@
+package Assignment4;
+
+
+import java.util.Scanner;
+
+public class FindMaxOccurringChar {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Input String : ");
+        String st=sc.next();
+        System.out.println("the Max of the duplicate string : ");
+        char ch[]=st.toCharArray();
+        int count;
+        for (int i=0;i<ch.length;i++){
+            count=1;
+            for(int j=i+1;j<ch.length;j++){
+                if(ch[i]==ch[j] && ch[i]!=0){
+                    count++;
+                    ch[j]=0;
+                }
+            }
+            if(count>1 && ch[i]!=0){
+                System.out.print(ch[i]);
+            }
+            }
+    }
+}
