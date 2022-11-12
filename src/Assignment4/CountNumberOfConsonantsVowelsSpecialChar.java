0b1bfd10d3b97b156f1c7c150029df0366f6438f
Sanjay <imsanju2k@gmail.com>
2022-11-12 15:42:55 +0530

    Question-1  CountNumberOfConsonantsVowelsSpecialChar

--- src/Assignment4/CountNumberOfConsonantsVowelsSpecialChar.java
+++ src/Assignment4/CountNumberOfConsonantsVowelsSpecialChar.java
@@ -1,7 +1,36 @@
 package Assignment4;
 
+import java.util.Scanner;
+
 public class CountNumberOfConsonantsVowelsSpecialChar {
     public static void main(String[] args) {
-     String str="java";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the input String along with SpecialCharacter");
+        String str=sc.next();
+        str=str.toLowerCase();
+        int vowels=0,consonants=0,special_chars=0,numbers=0;
+
+        for (int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                vowels++;
+            }
+            else if((ch>='a' && ch<='z')){
+                consonants++;
+            }
+            else if(ch>='0' && ch<='9'){
+                numbers++;
+            }
+            else if(ch=='$'||ch=='#'||ch=='/'||ch=='%'||ch=='*'||ch=='@'||ch=='!'){
+                special_chars++;
+            }
+
+
+        }
+        System.out.println("the number of vowels are present in the input string : " + vowels);
+        System.out.println("the number of consonants are present in the input string : " + consonants);
+        System.out.println("the number of numbers are present in the input string : " + numbers);
+        System.out.println("the number of special chars are present in the input string : " + special_chars);
+
     }
 }
