434bd64432b7f97d1d6f6ca5ad09b431a88ce7e3
Sanjay <imsanju2k@gmail.com>
2022-11-15 19:12:29 +0530

    Assignment 4

--- src/Assignment4/AnagramChecking.java
+++ src/Assignment4/AnagramChecking.java
@@ -1,4 +1,31 @@
 package Assignment4;
 
+
+import java.util.Arrays;
+
 public class AnagramChecking {
+    public static void main(String[] args) {
+
+        String str1="bad credit";
+        String str2="debit card";
+
+        str1=str1.replace(" ","");
+        str2=str2.replace(" ","");
+
+        char ch1[]=str1.toCharArray();
+        char ch2[]=str2.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        if(Arrays.equals(ch1,ch2)){
+            System.out.println("Its a Anagram sentence");
+        }
+        else {
+            System.out.println("Its not a Anagram sentence");
+        }
+
+
+
+    }
 }
